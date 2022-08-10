6dae249de17944d6e9fe74a43bab78410cd1b914
Your Name <jaheen07>
2022-08-10 17:15:37 +0600

    changed

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -343,11 +343,11 @@
                                 </div>
 
                                 <div class="card" style="height:21rem">
-<<<<<<< HEAD
+
                                     <img src="{{asset('uploads/banner')}}/{{App\models\banner::where('banner_number',2)->first('banner_image')}}" width="100%" height="100%">
-=======
-                                    <img src="{{asset('uploads/banner')}}/{{App\models\banner::where('banner_number',2)->get()->banner_image}}" width="100%" height="100%">
->>>>>>> cb968042a067633799f35d1443bd111fb7debfff
+
+                                    <img src="{{asset('uploads/banner')}}/{{App\models\banner::where('banner_number',2)->first('banner_image')}}" width="100%" height="100%">
+
                                     <div class="card-img-overlay">
                                         <a class="btn btn-danger mx-3 mb-3" href="{{url('/product_details')}}/{{App\models\banner::where('banner_number',3)->get()->first()->product_id}}" style="position:relative;top:80%">Start Buying</a>
                                     </div>
